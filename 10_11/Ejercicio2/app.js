abc289efec99dfd3b29f6ccaf4044fab90d426fa
Lukitas <lucas.r.abimorad@gmail.com>
2023-10-19 15:58:33 -0300

    wawa

--- 10_11/Ejercicio2/app.js
+++ 10_11/Ejercicio2/app.js
@@ -1,8 +1,23 @@
-const container = document.querySelector('#table_container')
+const container = document.querySelector("#table_container");
 
-for(let i = 1; i<= 16; i++){
-    container.innerHTML += `
-    <div class="table_elements">
-    `+i+`
-    </div>`
+for (let i = 1; i <= 16; i++) {
+  loadElements(i);
 }
+
+function loadElements(i) {
+  setTimeout(() => {
+    container.innerHTML += `<div class="table_elements pop-up">` + i + `</div>`;
+    setTimeout(() => {
+      container.children[i - 1].classList.remove("pop-up");
+    }, 500);
+  }, i * 600)
+}
+
+
+setTimeout(()=>{
+  for (let i = 0; i < 16; i++) {
+    container.children[i].classList.add('table_elements--active')
+    
+  }
+}, 10300)
+
